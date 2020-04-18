7f99c36731b8ac5290e5afcbeec4de61d541643c
Dehai Kong <imdehai@gmail.com>
2020-04-18 11:53:41 +0800

    Database and Migrations

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # a Django project demo
+
+## some django commands in terminal
+* ```django-admin  startproject your_project```
+* ```django-admin startapp your_apps```
+----
+* ```python manage.py runserver```
+* ```python manage.py createsuperuser```
+* ```python manage.py makemigrations```
+* ```python manage.py sqlmigrate your_apps your_migrations_num```
+* ```python manage.py migrate```
+* ```python manage.py shell```
--- myblog/views.py
+++ myblog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {'author': 'Dehai Kong',
-     'title': 'post 1',
-     'content': 'first content',
-     'date_posted': '17th April 2020'
-     },
-    {'author': 'victor kong',
-     'title': 'post 2',
-     'content': 'sencond content',
-     'date_posted': '18th April 2020'
-     },
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'myblog/home.html', context)
 
